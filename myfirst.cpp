0bdabb472049eae95e23cb5d3e57810fb21ced82
Zhili Zhao <zlzwork@outlook.com>
2025-04-04 20:49:53 +0900

    first modi

---
+++ myfirst.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    using namespace std;
+    cout <<"come up and C++ me some time.";
+    cout << endl;
+    cout <<"you wont regret it!" << endl;
+    return 0;
+}
