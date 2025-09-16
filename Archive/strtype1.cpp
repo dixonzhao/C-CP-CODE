259b7525a39fb67808424c2bab6a80f1cbd0cfac
DixonZhao <dixionzhao@gmail.com>
2025-09-16 14:36:43 +0900

    4.8

---
+++ Archive/strtype1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+int main(){
+    using namespace std;
+    char charr1[20];
+    char charr2[20] = "jaguar";
+    string str1;
+    string str2 = "panther";
+    charr1 = charr2;
+    str1 = str2;
+    string str3;
+    str3 = str1 + str2;
+    
+
+}
