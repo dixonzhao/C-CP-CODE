f8eceb13d426e1f2a58ec06370b99c0427c9f470
DixonZhao <dixionzhao@gmail.com>
2025-10-12 20:32:05 +0900

    CH4 end

--- addpntrs.cpp
+++ addpntrs.cpp
@@ -5,4 +5,25 @@ int main(){
     short stacks[3] = {3, 2, 1};
     
     double * pw = wages;
+    short * ps = &stacks[0];
+
+    cout << "pw = " << pw << ", *pw = " <<  *pw << endl;
+    pw = pw + 1;
+    cout << "add 1 to the pw pointer:\n";
+    cout << "pw = " << pw << ", *ps = " << *pw << "\n\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << endl;
+    ps = ps +1;
+    cout << "add 1 to the ps pointer:\n";
+    cout << "ps = " << ps << ", *ps = " << *ps <<"\n\n";
+
+    cout << "access two element with array notation\n";
+    cout << "stacks[0] = " << stacks[0]
+        << ", stacks[1] = " << stacks[1] << endl;
+    cout << "access two elements with pointer notation\n";
+    cout << "*stacks = " << *stacks 
+        << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+
+    cout << sizeof(wages) << " = size of wage array\n";
+    cout << sizeof(pw) << " = size of pw pointer\n";
+    return 0;
 }
