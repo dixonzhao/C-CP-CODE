6a1b38a0db5837ddce60e38ac1611c075e4f0956
Zhili Zhao <zlzwork@outlook.com>
2025-12-13 21:08:58 +0900

    CH5 end

--- textin3.cpp
+++ textin3.cpp
@@ -3,12 +3,11 @@ int main(){
     using namespace std;
     char ch;
     int count = 0;
-    cout << "Enter characters; enter # to quit:\n";
-    cin >> ch;
-    while (ch != '#'){
+    cin.get(ch);
+    while (cin.fail() == false){
         cout << ch;
         ++count;
-        cin >> ch;
+        cin.get(ch);
     }
     cout << endl << count << " characters read\n";
     return 0;
